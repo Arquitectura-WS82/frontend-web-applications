579f86c7fb7a89264f5520c5215566086e98c096
Anthony Botello S <89035933+AnthonyBotello4@users.noreply.github.com>
2022-09-24 21:33:02 -0500

    build: initial login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/pages/login/login.component';
+import { SignupP1Component } from './login/pages/signup/signup-p1/signup-p1.component';
+import { SignupP2Component } from './login/pages/signup/signup-p2/signup-p2.component';
+import { SignupP3Component } from './login/pages/signup/signup-p3/signup-p3.component';
+import { HeaderComponent } from './login/header/header.component';
+import { HomeCComponent } from './client/home-c/home-c.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'signup-p1', component: SignupP1Component },
+  { path: 'signup-p2', component: SignupP2Component },
+  { path: 'signup-p3', component: SignupP3Component },
+  { path: 'home-c', component: HomeCComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/pages/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSortModule } from '@angular/material/sort';
+import { HeaderComponent } from './login/header/header.component';
+import { FooterComponent } from './login/footer/footer.component';
+import { SignupP1Component } from './login/pages/signup/signup-p1/signup-p1.component';
+import { SignupP2Component } from './login/pages/signup/signup-p2/signup-p2.component';
+import { SignupP3Component } from './login/pages/signup/signup-p3/signup-p3.component';
+import { HeaderCComponent } from './client/header-c/header-c.component';
+import { HomeCComponent } from './client/home-c/home-c.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    SignupP1Component,
+    SignupP2Component,
+    SignupP3Component,
+    HeaderCComponent,
+    HomeCComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
